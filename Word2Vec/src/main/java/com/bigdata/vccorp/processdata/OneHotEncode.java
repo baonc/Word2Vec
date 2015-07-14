441944afa3b4b6201bc10e4a899a81ef83e87445
BaoNC <baonc93@gmail.com>
2015-07-14 09:26:06 +0700

    init one-hot encode

---
+++ Word2Vec/src/main/java/com/bigdata/vccorp/processdata/OneHotEncode.java
@@ -0,0 +1,15 @@
+package com.bigdata.vccorp.processdata;
+
+/**
+ * Class encode a word with one-hot<br>
+ * meaning they are vectors of length V (the size of the vocabulary) with a value of 1 at the
+ *  index corresponding to the word and zeros in all other indexes
+ * 
+ * @created 14 / 7 / 2015
+ * @author baonc
+ * @github: https://github.com/baonc/Word2Vec
+ *
+ */
+public class OneHotEncode {
+
+}
